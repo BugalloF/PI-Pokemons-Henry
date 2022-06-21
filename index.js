746e7bcb7d48163ac12390a3dff2faa337db14af
Fermin Bugallo <bugallof@gmail.com>
2022-06-21 19:50:02 -0300

    deployyyy

--- index.js
+++ index.js
@@ -17,13 +17,13 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const server = require('./api/src/app.js');
+const { conn } = require('./api/src/db.js');
 const{ PORT} = process.env
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
+  server.listen(PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
