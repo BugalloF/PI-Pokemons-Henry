178aabbf88a0ab958bd8d36080ae618afc90b43b
Fermin Bugallo <bugallof@gmail.com>
2022-06-21 20:10:18 -0300

    ola2

--- api/src/db.js
+++ api/src/db.js
@@ -5,6 +5,8 @@ const path = require('path');
 const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
+const { DATABASE_URL } = process.env;
+
 
 const sequelize = new Sequelize(
   DATABASE_URL,
