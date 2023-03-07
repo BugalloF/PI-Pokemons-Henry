f680f0867d564446cd3050b0b1a17524eb946e34
BugalloF <bugallof@gmail.com>
2023-03-07 17:48:16 -0300

    Railway

--- api/src/db.js
+++ api/src/db.js
@@ -3,26 +3,26 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_HOST,DB_NAME
 } = process.env;
 const { DATABASE_URL } = process.env;
 
 
-const sequelize = new Sequelize(
-  DATABASE_URL,
-  {
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-    dialectOptions:
-    {
-      ssl:
-      {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  },
-);
+let sequelize =
+  process.env.NODE_ENV === "production"
+    ?
+    new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: {
+          rejectUnauthorized: false
+        }
+      }
+    })
+    : new Sequelize(
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+        { logging: false, native: false }
+      );
 
 
 const basename = path.basename(__filename);
